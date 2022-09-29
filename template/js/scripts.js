2ad1a00d125b39fb55f0db0e7e81cc260783122e
SheilAPK <karaulov.z@mail.ru>
2022-09-29 09:43:46 +0300

    Block header

---
+++ template/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(
+    function(){
+        $('.header__burger, .close-phone-menu').click(
+            function (event) {
+                $('div.header__burger').toggleClass('header__burger_close');
+                $('header').toggleClass('phone');
+                $('.close-phone-menu').toggleClass('active');
+            }
+        );
+    }
+);
