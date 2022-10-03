a57071684c032022b028e2c99bc21557975dae15
SheilNoteBook <karaulov.z@mail.ru>
2022-10-03 06:58:10 +0300

    In work

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -8,5 +8,37 @@ $(document).ready(
                 $('body').toggleClass('lock');
             }
         );
+
+        /*
+        <div class="templates-main__item templates-item">
+            <div class="templates-item__img">
+                <img src="./template/img/templates/1-full.jpg" alt="Landing vr.1">
+            </div>
+            <div class="templates-item__name">Landing vr.1</div>
+        </div>
+        */
+
+
+        /* Заполнение карточек в template */
+        /*
+        const arr = Array (
+            ["./template/img/templates/1-full.jpg",
+            "Landing vr.1"],
+            ["./template/img/templates/2-full.jpg",
+            "Landing vr.2"]
+        )
+        
+        arr.forEach(element => {
+            let div = $("<div class='templates-main__item templates-item'>")
+            let divImg = $("<div class='templates-item__img'></div>")
+
+            div.append(divImg)
+
+            let img = document.createElement("img")
+            img.setAttribute("src", element[1])
+
+            console.log(div)
+        });
+        */
     }
 );
